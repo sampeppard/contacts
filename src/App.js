8e2d8b4f63aebca4b149654b8d0b8caabff174e8
Sam Peppard <SPeppard@urbn.com>
2018-02-10 23:35:24 -0500

    add files for new react project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+const contacts = [
+    {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+]
+
+class App extends Component {
+  render() {
+    return (
+        <ListContacts/>
+    )
+  }
+}
+
+export default App;
